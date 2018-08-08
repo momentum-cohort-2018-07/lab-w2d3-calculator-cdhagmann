8028ace141931c5b03d3bbcb2bc81c8b0d96df1d
Christopher Dean Hagmann <cdhagmann@gmail.com>
2018-08-08 16:15:14 -0400

    Added keybindings

--- main.js
+++ main.js
@@ -1,57 +1,63 @@
-var appendButtons = document.getElementsByClassName("append");
-for (var button of appendButtons) {
-    button.addEventListener("click", appendText);
+var display = document.getElementById("display");
+
+for (var button of document.getElementsByClassName("append")) {
+    button.addEventListener("click", buttonClick);
 }
 
 clear = document.getElementById('clear');
 clear.addEventListener("click", function () {
-    var display = document.getElementById("display");
     display.innerHTML = '';
 });
 
 equal = document.getElementById('equal');
-equal.addEventListener("click", function (e) {
-    console.log(e);
-    var display = document.getElementById("display");
-    console.log(display.innerText);
-    display.innerText += '\n' + math.round(math.eval(display.innerText), 3);
-    display.classList.add('results');
+equal.addEventListener("click", calculate);
+
+document.addEventListener('keypress', function () {
+    if (event.key === 'Enter') {
+        calculate();
+    } else if ('0123456789.+-*/'.includes(event.key)) {
+        appendText(event.key);
+    } else {
+        display.innerHTML = '';
+    }
 });
 
-function appendText(e) {
-    console.log(e);
+function calculate() {
+    display.classList.add('results');
+    try {
+        display.innerText += '\n' + math.round(math.eval(display.innerText), 3);
+        display.classList.add('results');
+    } catch (error) {
+        display.innerHTML = '';
+    }
+}
+
+function buttonClick(e) {
     var button = e.toElement;
-    var display = document.getElementById("display");
+    appendText(button.innerText)
+}
+
+function appendText(text) {
     if (display.classList.contains('results')) {
         display.classList.remove('results');
-        if (!button.classList.contains('operation')) {
-            display.innerText = '';
+        if ('+-*/'.includes(text)) {
+            display.innerText = display.innerText.split("\n")[1]; // store result if starting with operation
         } else {
-            display.innerText = display.innerText.split("\n")[1];
+            display.innerText = ''; // clear display if clicking on new number
         }
     }
 
-    // Replace special characters to make it work in
+    // Replace special characters to make math.eval work in
     var newText = '';
-    if (button.innerText === '÷') {
+    if (text === '÷') {
         newText = '/';
-    } else if (button.innerText === '×') {
+    } else if (text === '×') {
         newText = '*';
-    } else {
-        newText = button.innerText;
+    } else if (text === '.' && display.innerText.length === 0) {
+        newText = '0.';
+    } else if ('0123456789.+-*/'.includes(text)) {
+        newText = text;
     }
 
     display.innerText += newText;
 }
-
-
-
-// bear.addEventListener('click', bearSays);
-
-// var bearMessages = ["I love you", "Do you have any honey?", "Have you seen my hat?"];
-
-// function bearSays() {
-//     var msg = bearMessages[Math.floor(Math.random() * bearMessages.length)];
-//     var hardCodedMsg = bearMessages[0];
-//     document.querySelector('.message').innerText = msg;
-// }
